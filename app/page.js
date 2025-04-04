66858405e96518ebe3af920cac187657b722e117
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-04-04 19:20:48 +0530

    added

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className='bg-neutral-100'>
       <Home />
     </div>
   )
--- components/Home.jsx
+++ components/Home.jsx
@@ -2,11 +2,35 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div class>
-        <div>
+    <div className='flex flex-col items-center md:p-20 p-10 min-h-screen'>
+    <div className='flex justify-center items-start md:space-x-24 space-x-10'>
+       <div className='flex flex-col'>
+       <div className='bg-white py-1 w-full rounded-t-xl shadow flex items-center justify-between'>
+        <img src='/toolbar.svg' className='w-96' />
 
+       </div>
+        <div className='bg-neutral-400 p-1'>
+            <img src='/home.jpg' className='w-96' />
         </div>
-        <div></div>
+       </div>
+        <div className='flex flex-col md:gap-10 gap-4'>
+            <div className='text-black text-center font-medium'> 
+                <img src='/folder.png' className='w-24' />
+                <p>stories</p>
+            </div>
+            <div className='text-black text-center font-medium'>
+                <img src='/folder.png' className='w-24' />
+                <p>profile</p>
+            </div>
+            <div className='text-black text-center font-medium'>
+                <img src='/folder.png' className='w-24' />
+                <p>random</p>
+            </div>
+        </div>
+    </div>
+    <div className='font-[PPMondwest] text-black text-3xl pt-4 max-w-xl'>
+        Tag-team storytelling awaits! Sign up, take turns, and see where your creativity takes you.
+    </div>
     </div>
   )
 }
